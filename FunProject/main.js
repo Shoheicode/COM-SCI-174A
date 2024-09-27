245c6c08ce837c2301c6c55b3da6e18adcd0b14e
ShoheiCode <ucodejasonirie@gmail.com>
2024-09-27 12:38:21 -0700

    made a cube trapped in lines

--- FunProject/main.js
+++ FunProject/main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -14,9 +15,11 @@ camera.position.z = 5;
 const materialLine = new THREE.LineBasicMaterial( { color: 0x0000ff } );
 
 const points = [];
-points.push( new THREE.Vector3( - 10, 0, 0 ) );
-points.push( new THREE.Vector3( 0, 10, 0 ) );
-points.push( new THREE.Vector3( 10, 0, 0 ) );
+points.push( new THREE.Vector3( -5, -3, 0 ) );
+points.push( new THREE.Vector3( 5, -3, 0 ) );
+points.push( new THREE.Vector3( 5, 3, 0 ) );
+points.push( new THREE.Vector3( -5, 3, 0 ) );
+points.push( new THREE.Vector3( -5, -3, 0 ) );
 
 const geometryLine = new THREE.BufferGeometry().setFromPoints( points );
 
@@ -31,25 +34,32 @@ document.body.appendChild( renderer.domElement );
 let speedX = 0.1;
 let speedY = 0.1;
 
+// Adding controls for the camera
+const controls = new OrbitControls(camera, renderer.domElement);
+camera.position.set(0, 5, 10); // Where the camera is.
+controls.target.set(0, 5, 0); // Where the camera is looking towards.
+
 function animate() {
 	renderer.render( scene, camera );
 
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
-    if(cube.position.x < -5){
+    if(cube.position.x < -4.5){
         speedX = Math.random()*0.09 + 0.01
-    }else if(cube.position.x > 5){
+    }else if(cube.position.x > 4.5){
         speedX = Math.random()*0.09 + 0.01
         speedX *=-1;
     }
-    if(cube.position.y < -3){
+    if(cube.position.y < -2.5){
         speedY = Math.random()*0.09 + 0.01
-    }else if(cube.position.y > 3){
+    }else if(cube.position.y > 2.5){
         speedY = Math.random()*0.09 + 0.01
         speedY *=-1;
     }
     
     cube.position.x += speedX;
     cube.position.y += speedY;
+
+    renderer.render( scene, camera );
 }
 renderer.setAnimationLoop( animate );
