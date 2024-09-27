4961e79d8640cadee816fede899e28127d43cc71
ShoheiCode <ucodejasonirie@gmail.com>
2024-09-27 12:16:31 -0700

    updated to add rotation

--- FunProject/main.js
+++ FunProject/main.js
@@ -16,5 +16,8 @@ document.body.appendChild( renderer.domElement );
 
 function animate() {
 	renderer.render( scene, camera );
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
 }
 renderer.setAnimationLoop( animate );
