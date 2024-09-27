77968e613d383bb6859a44f2f2df966c7058c7fa
ShoheiCode <ucodejasonirie@gmail.com>
2024-09-27 11:47:43 -0700

    updated code

--- Assignment1/main.js
+++ Assignment1/main.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-const scene = new THREE.Scene();
+const scene = new THREE.Scene(); // Generates the scene that is used in the code
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
