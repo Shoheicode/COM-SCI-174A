919f6ff500285e00c73d363345d7b065f8dda8bf
ShoheiCode <ucodejasonirie@gmail.com>
2024-09-29 13:09:39 -0700

    updated page

--- FunProject/player.js
+++ FunProject/player.js
@@ -1,3 +1,11 @@
 class Player {
-    
+    constructor(player, posX, posY){
+        this.player = player;
+        this.player.position.x = posX;
+        this.player.position.y = posY;
+    }
+
+    movePlayer(x,y){
+        this.player.position.x = x;
+    }
 }
