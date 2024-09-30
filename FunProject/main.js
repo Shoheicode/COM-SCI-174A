7b79e585186942ccbe629f794eac12bc2e67bf5e
ShoheiCode <ucodejasonirie@gmail.com>
2024-09-30 12:05:47 -0700

    added in the function to the game

--- FunProject/main.js
+++ FunProject/main.js
@@ -151,6 +151,10 @@ function animate() {
         player.position.y += speedPlayer;
     }
 
+    if(touchingEachOther(player, ball)){
+        
+    }
+
     speedCap+=0.00001
     renderer.render( scene, camera );
 }
