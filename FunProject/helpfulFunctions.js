f0e15d19aa56be710e4b5681e946c8321c8bc693
ShoheiCode <ucodejasonirie@gmail.com>
2024-09-30 12:15:16 -0700

    update page

--- FunProject/helpfulFunctions.js
+++ FunProject/helpfulFunctions.js
@@ -3,6 +3,10 @@ function touchingPlayerBallTouch(player, ball, boxArea){
         if(player.position.y+boxArea[1] > ball.position.y && player.position.x + boxArea[0] < ball.position.x ){
             return true;
         }
+    }else{
+        if(player.position.y-boxArea[1] < ball.position.y && player.position.x + boxArea[0] < ball.position.x ){
+            return true;
+        }
     }
 }
 
