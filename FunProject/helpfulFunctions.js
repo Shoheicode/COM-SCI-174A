e1094b2af613efec0f32fa2f5e06ea4f85f939d8
ShoheiCode <ucodejasonirie@gmail.com>
2024-09-30 17:41:02 -0700

    updated pos

--- FunProject/helpfulFunctions.js
+++ FunProject/helpfulFunctions.js
@@ -1,6 +1,6 @@
 function touchingPlayerBallTouch(player, ball, boxArea){
     console.log("PLAYER X: " + player.position.x + boxArea[0]);
-    console.log("Ball X: " + player.position.x + boxArea[0]);
+    console.log("Ball X: " + ball.position.x);
     if(player.position.y < ball.position.y){
         if(player.position.y+boxArea[1] > ball.position.y && player.position.x + boxArea[0] < ball.position.x ){
             return true;
