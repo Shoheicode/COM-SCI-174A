3386c3601353bc8769ec7be0a146b5f16e548dc9
ShoheiCode <ucodejasonirie@gmail.com>
2024-10-01 19:12:53 -0700

    updated to hopefully work a bit better

--- FunProject/helpfulFunctions.js
+++ FunProject/helpfulFunctions.js
@@ -3,12 +3,13 @@ function touchingPlayerBallTouch(player, ball, boxArea){
     // console.log("Ball X: " + ball.position.x);
 
     //NEED TO DO SOME GEOMETRY
+
     if(player.position.y < ball.position.y){
-        if(player.position.y+boxArea[1] > ball.position.y && player.position.x + boxArea[0] > ball.position.x ){
+        if(player.position.y+boxArea[1] > ball.position.y && player.position.x + boxArea[0] > ball.position.x-ball.radius ){
             return true;
         }
     }else{
-        if(player.position.y-boxArea[1] < ball.position.y && player.position.x + boxArea[0] > ball.position.x ){
+        if(player.position.y-boxArea[1] < ball.position.y && player.position.x + boxArea[0] > ball.position.x -ball.radius){
             return true;
         }
     }
