7a768a8a844754bee958fa13d2be6f23890e338b
ShoheiCode <ucodejasonirie@gmail.com>
2024-10-02 14:41:10 -0700

    removed repeated code

--- FunProject/main.js
+++ FunProject/main.js
@@ -129,7 +129,6 @@ let speedCap = 0.1
 const geometry = new THREE.BoxGeometry(1, 2, 3)
 geometry.computeBoundingBox()
 
-geometry.computeBoundingBox()
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
 const mesh = new THREE.Mesh(geometry, material)
 mesh.position.set(4, 1, 0)
