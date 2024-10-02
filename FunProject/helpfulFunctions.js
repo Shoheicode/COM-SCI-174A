e81023176132e703d6a9ae9be030db571211630f
ShoheiCode <ucodejasonirie@gmail.com>
2024-10-01 19:10:02 -0700

    need to geometry

--- FunProject/helpfulFunctions.js
+++ FunProject/helpfulFunctions.js
@@ -1,6 +1,8 @@
 function touchingPlayerBallTouch(player, ball, boxArea){
     // console.log("PLAYER X: " + player.position.x + boxArea[0]);
     // console.log("Ball X: " + ball.position.x);
+
+    //NEED TO DO SOME GEOMETRY
     if(player.position.y < ball.position.y){
         if(player.position.y+boxArea[1] > ball.position.y && player.position.x + boxArea[0] > ball.position.x ){
             return true;
