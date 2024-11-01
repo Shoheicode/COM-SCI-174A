3cf51363545a13ff3f5d6a6319ff7c0a3cd61afb
ShoheiCode <ucodejasonirie@gmail.com>
2024-11-01 14:29:38 -0700

    updated

--- Assignment3/main.js
+++ Assignment3/main.js
@@ -151,7 +151,7 @@ animate();
 
 // TODO: Implement the Gouraud Shader for Planet 2
 function createGouraudMaterial(materialProperties) {  
-    console.log(materialProperties)
+    // console.log(materialProperties)
     const numLights = 1;  
     // TODO: Implement the Vertex Shader in GLSL
     let vertexShader = `
@@ -549,6 +549,7 @@ function animate() {
         if(index == 2){
             const wobbleX = 0.05 * Math.sin(time * 2.0);
             const wobbleZ = 0.05 * Math.sin(time * 1.5);
+            console.log(wobbleX)
             let mod2 = new THREE.Matrix4()
             mod2.multiply(rotationMatrixZ(wobbleZ*(10)), mod2)
             mod2 = mod2.multiply(rotationMatrixX(wobbleX*(10)), mod2)
